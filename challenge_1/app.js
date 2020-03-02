a6ff0bfed9f849d9b28b9fcd5c460c3ace1994e5
fasterthansky <fasterthansky@gmail.com>
2020-03-02 15:09:51 -0800

    completed tictactoe

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,58 @@
-console.log("workS!")
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+let board;
+let turn = 'X';
+let win;
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
+//event-listeners
+document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('reset-button').addEventListener('click', init);
+//functions
+function getWinner() {
+  let winner = null;
+  winningCombos.forEach(function(combo, index) {
+  if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) {
+    winner = board[combo[0]];
+  }
+  });
+  return winner ? winner : board.includes('') ? null : 'T';
+};
+
+function handleTurn(event) {
+  let idx = squares.findIndex(function (square) {
+    return square === event.target;
+  });
+  board[idx] = turn;
+  turn = turn === 'X' ? 'O' : 'X';
+  win = getWinner();
+  render();
+};
+
+function init() {
+  board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+  ];
+  render();
+};
+
+function render() {
+  board.forEach(function (val, idx) {
+    squares[idx].textContent = val;
+  });
+  messages.textContent = win === 'T' ? `Tie!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+};
+
+init();
