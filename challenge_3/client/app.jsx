a01abf6f14db2c9b7b2d77791fe7ca1cb6b5064b
fasterthansky <fasterthansky@gmail.com>
2020-03-04 15:45:58 -0800

    setup server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,37 @@
 
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formView: 0,
+      forms: props.props,
+      formData: {
+        'Name': '',
+        'Email': '',
+        'Password': '',
+        'Line 1': '',
+        'Line 2': '',
+        'City': '',
+        'State': '',
+        'Zipcode': '',
+        'Credit Card Number': '',
+        'Expiration Date': '',
+        'CVV': '',
+        'Billing Zipcode': ''
+      }
+    };
+  }
+
+  // let forms = {
+  //   1: ['Name', 'Email', 'Password'],
+  //   2: ['Line 1', 'Line 2', 'City', 'State', 'Zipcode'],
+  //   3: ['Credit Card Number', 'Expiration Date', 'CVV', 'Billing Zipcode']
+  // };
+
+
+  // ReactDOM.render(
+  //   <App props={forms} />,
+  //   document.getElementById('app')
+  // );
